07043019b8fbfa138bbf4a7931a78130f357ad95
adipatel007 <adi.patel1331@gmail.com>
2026-01-29 16:35:16 +0530

    Delete API directory

--- API/models/fileuploadModel.js
+++
@@ -1,9 +0,0 @@
-class FileModel {
-    constructor(name, type, url) {
-        this.name = name;
-        this.type = type;
-        this.url = url;
-    }
-}
-
-module.exports = FileModel;
--- API/routes/dashboardRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const DashboardController = require('../controllers/dashboardController');
-const { auth } = require('../middlewares/auth.js');
-
-router.get('/dashboard',auth, DashboardController.adminDashboard);
-
-module.exports = router;
