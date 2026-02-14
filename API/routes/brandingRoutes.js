e4be3daa37409749cd0aa9973d4351f3385d88e5
adipatel007 <adi.patel1331@gmail.com>
2026-02-14 18:25:06 +0530

    Delete API directory

--- API/routes/brandingRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const brandsController = require('../controllers/brandsController');
-
-// Public branding endpoint for all panels
-router.get('/:brandCode', brandsController.getBrandingByCode);
-
-module.exports = router;
